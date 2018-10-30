6fd219f3ac1aa0a696156d105a09cc43e58d5867
Michelle Tang <mtang1@stuy.edu>
2018-10-30 13:16:28 -0400

    finished modifty size

--- stat.c
+++ stat.c
@@ -1,20 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include <sys/stat.h>
 #include <time.h>
 
+char * readable_form(long size){
+  int index = 0;
+  char * sizes[] = {"B", "KB", "MB", "GB"};
+  while (size >= 1024){
+    size /= 1024;
+    index+=1;
+  }
+  return sizes[index];
+}
 
 int main(){
   struct stat fileStats;
   stat("stat.c", &fileStats);
-  printf("Size: %ld\n", fileStats.st_size);
+  float size = (float) fileStats.st_size;
+  char * place = readable_form(size);
+  while (size >= 1024){
+    size /= 1024;
+  }
+  printf("Size: %f %s \n", size , place );
   printf("Permissions: %o\n", fileStats.st_mode);
-  printf("Last Access Time: %s", ctime(&fileStats.st_atime));
-  printf("Last Modify Time: %s", ctime(&fileStats.st_mtime));
-  printf("%s\n", "HI" );
+  printf("Last Accessed Time: %s", ctime(&fileStats.st_atime));
+  printf("Last Modified Time: %s", ctime(&fileStats.st_mtime));
+
 
   return 0;
 }
